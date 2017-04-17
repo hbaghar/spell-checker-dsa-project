b5ef3d9646f421a042c8093f59fa3aef8e203b7a
hbaghar <hriday.baghar@gmail.com>
2017-04-18 01:23:47 +0530

    Wordlist and Trie program
    Uploaded wordlist which is read by trie program. The words from the wordlist are stored in a trie data structure.
    Next steps:
    - Implement a trie traversal algorithm that states whether a word was found or not
    - Find time taken for searching
    - Implement suggestion algorithm and find time taken for that as well

---
+++ trie_dict.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<stdlib.h>
+using namespace std;
+
+class TrieNode{
+    public:
+		bool isLeaf;
+        class TrieNode* nextChar[26];									//26 possible next pointers of each character
+        TrieNode(){														//initializing all values to defualt
+            for(int i=0;i<26;i++){
+                nextChar[i]=NULL;
+            }
+            isLeaf=false;
+        }
+};
+
+void insert(class TrieNode *root, string word)
+{
+    int length = word.length();
+    int index;
+    TrieNode *ptrTrav= new TrieNode;									//creating node to add each character to trie
+	ptrTrav=root;
+    for (int level=0; level<length; level++)
+    {
+        index = (int)word[level]-97;									//normalizing ascii value between 0-25 (26 letters in the alphabet)
+        if (!ptrTrav->nextChar[index])		
+            ptrTrav->nextChar[index] = new TrieNode;					//create new node if not created already for same prefix
+ 
+        ptrTrav = ptrTrav->nextChar[index];
+    }
+    ptrTrav->isLeaf = true; 											// mark last node as leaf
+}
+
+int main(){
+    ifstream in("wordlist.txt");
+    string word,key;
+    TrieNode *root= new TrieNode;
+    while(in){															//read all words from wordlist and store in trie
+        in>>word;
+        insert(root,word);
+    }
+    in.close();
+    cout<<"Trie Construction Successful"<<endl;
+    cout<<"Enter the word to be searched for : ";
+    cin>>key;
+    return 0;
+}
